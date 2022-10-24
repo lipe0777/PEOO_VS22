7c2aab9f44e5b7958f6a87a07e9dc0602dc68a57
Felipe Gabriel Emídio de Freitas <e.gabriel@academico.ifrn.edu.br>
2022-10-24 15:47:05 -0300

    lista 04 ex:1

---
+++ exercicio lista 04/aula 04/Abastecimento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_04
+{
+    class Abastecimento
+    {
+        private double valorLitro;
+        private double valorPago;
+        private double kmRodados;
+
+        public void SetValorLitro(double valorLitro){
+            if (valorLitro > 0) this.valorLitro = valorLitro;
+        }
+        public void SetValorPago(double valorPago){
+            if (valorPago > 0) this.valorPago = valorPago;
+        }
+        public void SetKmRodados(double kmRodados){
+            if (kmRodados > 0) this.kmRodados = kmRodados;
+        }
+        public double MediaKmLitro(){
+            double numLitros = valorPago / valorLitro;
+            return kmRodados / numLitros;
+        }
+        public double MediaReaiskm(){
+            return valorPago / kmRodados;
+        }
+    }
+}
